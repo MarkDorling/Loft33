fb0beb55da588f9b30a6f9813dbb68a1e595c267
Mark Dorling <mdorling1@gmail.com>
2025-08-26 09:59:32 +1000

    Vercel tracking added https://vercel.com/docs/analytics/quickstart#add-the-analytics-component-to-your-app

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'Loft 33',
@@ -12,13 +13,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     // Set your theme here: theme-1 .. theme-1
-    <html lang="en" className="theme-3
-  
-  ">
+    <html lang="en" className="theme-3">
       <body className="min-h-screen flex flex-col bg-primary text-primary">
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
+        <Analytics /> {/* ← added */}
       </body>
     </html>
   );
