ae3a15466bdf7889c5f20dfccf3afdf5e80a7a4f
Mark Dorling <mdorling1@gmail.com>
2025-08-26 10:06:32 +1000

    Vercel Speed Insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: 'Loft 33',
